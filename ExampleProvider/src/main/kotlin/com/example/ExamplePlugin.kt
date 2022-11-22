2ebc6f29f9a0571767c717873a04fa2832dd563e
Tolga077 <105724628+Tolga077@users.noreply.github.com>
2022-11-22 17:53:08 +0300

    Update ExamplePlugin.kt

--- ExampleProvider/src/main/kotlin/com/example/ExamplePlugin.kt
+++ ExampleProvider/src/main/kotlin/com/example/ExamplePlugin.kt
@@ -1,13 +1,14 @@
-package com.example
+
+package com.hexated2
 
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
 import android.content.Context
 
 @CloudstreamPlugin
-class TestPlugin: Plugin() {
+class HdfilmcehennemiPlugin: Plugin() {
     override fun load(context: Context) {
         // All providers should be added in this manner. Please don't edit the providers list directly.
-        registerMainAPI(ExampleProvider())
+        registerMainAPI(Hdfilmcehennemi())
     }
 }
